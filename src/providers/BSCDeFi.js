5509e20be0d9433db21f97168e65b54527bfb9b6
Victor Le <coac@protonmail.com>
2021-03-15 14:11:25 +0100

    fix: reward per year for pcs clone (#22)
    * feat: add pcs reward per year
    
    * refactor: move lp class to file
    
    * fix: ignore failed transactions
    
    * fix: reward per year calculation

--- src/providers/BSCDeFi.js
+++ src/providers/BSCDeFi.js
@@ -21,18 +21,18 @@ class BSCDeFi {
 
     this.defiPlatforms = {
       acryptos: new PancakeSwapCloneChef(web3, '0x96c8390BA28eB083A784280227C37b853bc408B7', 'pendingSushi', 'ACS'),
-      alpaca: new PancakeSwapCloneChef(web3, '0xA625AB01B08ce023B2a342Dbb12a16f2C8489A8F', 'pendingAlpaca'),
+      alpaca: new PancakeSwapCloneChef(web3, '0xA625AB01B08ce023B2a342Dbb12a16f2C8489A8F', 'pendingAlpaca', 'ALPACA'),
       apeSwap: new PancakeSwapCloneChef(web3, '0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9', 'pendingCake', 'BANANA'),
       autoFarm: new AutoFarmChef(web3),
       bVault: new BVaultChef(web3),
       cafeSwap: new PancakeSwapCloneChef(web3, '0xc772955c33088a97D56d0BBf473d05267bC4feBB', 'pendingCake', 'BREW'),
-      coralFarm: new PancakeSwapCloneChef(web3, '0x713e34640ef300a0B178a9688458BbA8b1FA35A7', 'pendingCrl'),
-      goose: new PancakeSwapCloneChef(web3, '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57', 'pendingEgg'),
+      coralFarm: new PancakeSwapCloneChef(web3, '0x713e34640ef300a0B178a9688458BbA8b1FA35A7', 'pendingCrl', 'CRL'),
+      goose: new PancakeSwapCloneChef(web3, '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57', 'pendingEgg', 'EGG'),
       kebab: new PancakeSwapCloneChef(web3, '0x76FCeffFcf5325c6156cA89639b17464ea833ECd', 'pendingCake', 'KEBAB'),
       midasGold: new MidasGoldChef(web3),
       pancakeSwap: new PancakeSwapChef(web3),
       ramen: new PancakeSwapCloneChef(web3, '0x97dd424b4628c8d3bd7fcf3a4e974cebba011651', 'pendingCake', 'RAMEN'),
-      saltSwap: new PancakeSwapCloneChef(web3, '0xB4405445fFAcF2B86BC2bD7D1C874AC739265658', 'pendingSalt'),
+      saltSwap: new PancakeSwapCloneChef(web3, '0xB4405445fFAcF2B86BC2bD7D1C874AC739265658', 'pendingSalt', 'SALT'),
       slime: new PancakeSwapCloneChef(web3, '0x4B0073A79f2b46Ff5a62fA1458AAc86Ed918C80C', 'pendingReward', 'SLIME')
     }
   }
