857a4f9d13e19c838809b948b98a9749c5ed0db6
Coac <coac@protonmail.com>
2021-03-27 12:48:11 +0100

    feat: add valuedefi vfarm

--- src/providers/bsc/PancakeSwapCloneChef.js
+++ src/providers/bsc/PancakeSwapCloneChef.js
@@ -4,13 +4,22 @@ const MasterChef = require('./MasterChef')
 const pancakeSwapABI = require('../resources/abis/pancakeSwap.json')
 
 class PancakeSwapCloneChef extends MasterChef {
-  constructor (web3, address, pendingMethodName, tokenSymbol) {
+  constructor (web3, address, pendingMethodName, tokenSymbol, rewardPerBlockName = '') {
     const ABISymbol = pendingMethodName.replace('pending', '').toLowerCase()
-
+    rewardPerBlockName = rewardPerBlockName !== '' ? rewardPerBlockName : ABISymbol + 'PerBlock'
     const newChefABI = pancakeSwapABI
       .map(replaceABIMap('pendingCake', pendingMethodName))
-      .map(replaceABIMap('cakePerBlock', ABISymbol + 'PerBlock'))
+      .map(replaceABIMap('cakePerBlock', rewardPerBlockName))
     super(web3, newChefABI, address, pendingMethodName, tokenSymbol)
+    this.rewardPerBlockName = rewardPerBlockName
+  }
+
+  async getRewardPerBlock () {
+    try {
+      return await this.contract.methods[this.rewardPerBlockName]().call()
+    } catch (e) {
+      return await MasterChef.prototype.getRewardPerBlock.call(this)
+    }
   }
 }
 
